cdff58876497465aee1d590452e26b2cde158fbf
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2019-12-04 12:58:04 +0200

    the flow of the app work just fine

--- public/app.js
+++
@@ -1,8 +0,0 @@
-`use strict`;
-$(document).ready(function () {
-    $('#update_form').hide();
-    $('#update_btn').on('click', function () {
-        $('#update_form').toggle();
-    });
-    $('h2').text('textString');
-});
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 `use strict`;
 
-/// packages
+
+///// packages \\\\\
+
 require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
 const pg = require('pg');
 const { Translate } = require('@google-cloud/translate').v2;
+const methodOverride = require('method-override');
 const unirest = require("unirest");
+
 
 const translate = new Translate();
 const client = new pg.Client(process.env.DATABASE_URL)
@@ -19,7 +23,17 @@ app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+// Middleware to handle PUT and DELETE
+app.use(methodOverride((request, response) => {
+    if (request.body && typeof request.body === 'object' && '_method' in request.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = request.body._method;
+        delete request.body._method;
+        return method;
+    }
+}))
 
+///// Routs and Function call \\\\\
 app.get('/', proofOfLife);
 app.get('/dictionary', getFromDictionary)
 app.post('/info', getInfo)
@@ -30,13 +44,17 @@ app.get('/test', data)
 app.get('/main', renderHome)
 app.post('/main', getRandomWord)
 app.put('/update/:card_id', updateCard)
+app.post('/updateCard/:card_id', getOneCard)
+app.delete('/delete/:card_id', deletCard)
 
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
+
+///// The main functions \\\\\
+
 function data(req, res) {
 
-    console.log('testing');
     const app_id = "d6324635"; // insert your APP Id
     const app_key = "285981e5b60037743d3e6301a5a386a3"; // insert your APP Key
     let url = `https://od-api.oxforddictionaries.com/api/v2/entries/en-us/ball?fields=pronunciations&strictMatch=false`;
@@ -50,16 +68,6 @@ function data(req, res) {
         .catch(console.error);
 }
 
-// function collection(req, res) {
-//     let url = `https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf`;
-//     superagent.get(url)
-//         .set('x-rapidapi-key', "861a23e564msh9e59c3b41ee0870p1bc725jsn80dfae1d8670")
-//         .set('x-rapidapi-host', "wordsapiv1.p.rapidapi.com")
-//         .then(data => {
-//             res.status(200).json(data.body)
-//         })
-//         .catch(console.error);
-// }
 
 function getInfo(req, res) {
     const app_id = "d6324635"; // insert your APP Id
@@ -85,9 +93,11 @@ function getRandomWord(req, res) {
     console.log('test', index)
     res.render('pages/main', { randword: file.word[index] })
 }
+
 function renderHome(req, res) {
     res.render('pages/introduction')
 }
+
 function proofOfLife(req, res) {
     res.status(200).send(`hello form our app`)
     // res.render('pages/info')
@@ -120,18 +130,38 @@ function NewWordForm(req, res) {
 }
 
 function addNewWord(req, res) {
+    let { term, description, image_url, notes } = req.body
     let SQL = `INSERT INTO words (term, description, image_url, notes) VALUES ($1,$2,$3,$4) RETURNING *`;
-    let values = ['dog', 'mammal animal', 'https://i.dailymail.co.uk/1s/2019/11/23/09/21370544-7717313-image-a-1_1574501083030.jpg', 'hello doggoy'];
+    let values = [term, description, image_url, notes]
     client.query(SQL, values)
-        .then(results => {
-            res.json(results.rows);
-        })
+        .then(res.redirect('/dictionary'))
+}
+function getOneCard(req, res) {
+    res.render('pages/updateForm', { card: req.body })
+    // .catch(handleError);
 }
 
 function updateCard(req, res) {
 
+    let { term, description, image_url, notes } = req.body
+    let SQL = 'UPDATE words SET term=$1, description=$2, image_url=$3, notes=$4 WHERE id=$5'
+    let values = [term, description, image_url, notes, req.params.card_id]
+    console.log('hello', req.params);
+    console.log('values', values)
+;
+    client.query(SQL, values)
+        .then(res.redirect(`/dictionary`))
 }
 
+function deletCard(req, res) {
+    let SQL = 'DELETE FROM words WHERE id=$1'
+    let values = [req.params.card_id]
+    client.query(SQL, values)
+        .then(res.redirect(`/dictionary`))
+}
+
+
+///// Erorr Sections \\\\\
 function errorHandler(error, req, res) {
     res.status(500).send('We R Sorry')
 }
@@ -140,6 +170,8 @@ function notFoundHandler(req, res) {
     res.status(404).send('WHERE ARE YOU GOING!!')
 }
 
+
+///// Listening to the app \\\\\
 client.connect()
     .then(() => app.listen(PORT, () => {
         console.log(`Welcome aboard on port ${PORT}`);
